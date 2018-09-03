a8e29eeb6d22d27bfad6a6f3353a854805c1cc0a
tprax <tprax01@gmail.com>
2018-09-03 16:05:48 -0600

    Add main contactlist page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,13 @@
 class ContactsController < ApplicationController
   def index
+    @contacts = Contact.all
   end
 
   def show
+    @contact = Contact.find(params[:id])
   end
 
   def new
+    @contact = Contact.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'contacts/index'
-  get 'contacts/show'
-  get 'contacts/new'
-  get 'index/show'
-  get 'index/new'
+  resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
