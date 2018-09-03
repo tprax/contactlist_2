5a59d6206ccfb2fcca0d4a2940d75356cd05b241
tprax <tprax01@gmail.com>
2018-09-03 15:38:37 -0600

    updated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'contacts/index'
+  get 'contacts/show'
+  get 'contacts/new'
+  get 'index/show'
+  get 'index/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/index_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class IndexControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get index_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get index_new_url
+    assert_response :success
+  end
+
+end
